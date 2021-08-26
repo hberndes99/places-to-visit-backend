d8fb9791c00bda38a070aaad2f0a5825f00bce46
Harriette Berndes <harrietteb99@gmail.com>
2021-08-26 10:14:08 +0100

    models set up

--- places_to_visit/places/models.py
+++ places_to_visit/places/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+# dango has built in ORM
+# db structure represented as classes
 
-# Create your models here.
+
+
+class WishList(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class MapAnnotationPoint(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    latitude = models.IntegerField()
+    longitude = models.IntegerField
+    number = models.CharField(max_length=10)
+    streetAddress = models.CharField(max_length=50)
+    wishList = models.ForeignKey(WishList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- places_to_visit/places/views.py
+++ places_to_visit/places/views.py
@@ -4,3 +4,27 @@ from django.http import HttpResponse
 # Create your views here.
 def hello(request):
     return HttpResponse('<h1>hello</h1>')
+
+
+
+
+#GET all map points
+# '/'
+
+#GET all wish lists
+# '/wishlists'
+
+#GET map points for a specified wish list
+# '/wishlists/id/savedplaces'
+
+#POST new wish list
+# '/wishlists'
+
+#POST new map annotation point, related to a specific wish list
+# '/wishlists/id/savedplaces
+
+#DELETE wish list and all related map points
+# '/wishlists/id
+
+#DELETE map point from specific wish list
+# '/wishlists/id/savedplaces/id
